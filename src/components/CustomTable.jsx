b57a3936e4962264353ba292a34dd456c19faf6d
Opanpan <fanalriansyah@gmail.com>
2022-08-09 01:25:12 +0700

    Handle delete item

--- src/components/CustomTable.jsx
+++ src/components/CustomTable.jsx
@@ -1,11 +1,16 @@
 import { Table, Row, Col, Button } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteItem } from '../store/features/storageSlice';
 
 
 export default function CustomTable() {
-    const items = useSelector(
-        (state) => state.storage.items
-    );
+  const dispatch = useDispatch();
+
+  const items = useSelector((state) => state.storage.items);
+
+  const handleDeleteItem = (id) => {
+    dispatch(deleteItem(id))
+  };
 
   return (
     <Table striped bordered hover>
@@ -19,28 +24,33 @@ export default function CustomTable() {
         </tr>
       </thead>
       <tbody>
-      {items.map((x, i) => {
-        return (
+        {items.map((x, i) => {
+          return (
             <tr key={i}>
-          <td className='text-center'>{x.id}</td>
-          <td className='text-center'>{x.name}</td>
-          <td className='text-center'>Rp. {x.price}</td>
-          <td className='text-center'>{x.quantity} Pcs</td>
-          <td>
-            <Row>
-              <Col className='d-flex justify-content-center'>
-                <Button size='sm'>Update</Button>
-              </Col>
-              <Col className='d-flex justify-content-center'>
-                <Button className='mt-5 mt-sm-0' size='sm' variant='danger'>
-                  Delete
-                </Button>
-              </Col>
-            </Row>
-          </td>
-        </tr>
-        )
-      })}
+              <td className='text-center'>{x.id}</td>
+              <td className='text-center'>{x.name}</td>
+              <td className='text-center'>Rp. {x.price}</td>
+              <td className='text-center'>{x.quantity} Pcs</td>
+              <td>
+                <Row>
+                  <Col className='d-flex justify-content-center'>
+                    <Button size='sm'>Update</Button>
+                  </Col>
+                  <Col className='d-flex justify-content-center'>
+                    <Button
+                      onClick={() => handleDeleteItem(x.id)}
+                      className='mt-5 mt-sm-0'
+                      size='sm'
+                      variant='danger'
+                    >
+                      Delete
+                    </Button>
+                  </Col>
+                </Row>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
--- src/store/features/storageSlice.js
+++ src/store/features/storageSlice.js
@@ -14,9 +14,16 @@ export const storageSlice = createSlice({
       state.totalCreatedItem += 1;
       state.items.push(data.payload);
     },
+    deleteItem: (state, data) => {
+      let index = 0;
+      state.items.forEach((el, i) =>
+        el.id === data.payload ? (index = i) : 0
+      );
+      state.items.splice(index, 1);
+    },
   },
 });
 
-export const { addItem } = storageSlice.actions;
+export const { addItem, deleteItem } = storageSlice.actions;
 
 export default storageSlice.reducer;
